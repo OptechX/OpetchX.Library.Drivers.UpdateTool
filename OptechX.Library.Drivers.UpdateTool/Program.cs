0896b8be85dd2be67bc134f9d6758920e95be2f7
Danijel-James Wynyard <danijel@repasscloud.com>
2023-08-07 23:13:05 +1000

    update to v1.2.4

--- OptechX.Library.Drivers.UpdateTool/Program.cs
+++ OptechX.Library.Drivers.UpdateTool/Program.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using System.Xml;
 using Microsoft.VisualBasic.FileIO;
 using OptechX.Library.Drivers.UpdateTool.Models;
 
@@ -13,7 +14,7 @@ namespace OptechX.Library.Drivers.UpdateTool
         {
             if (args.Any(arg => arg.Contains("--version")))
             {
-                Console.WriteLine("Version: 1.1.3");
+                Console.WriteLine("Version: 1.2.4");
                 return;
             }
 
@@ -74,6 +75,157 @@ namespace OptechX.Library.Drivers.UpdateTool
             // Now you have the csvFilePath, xmlFilePath, and oem values.
             // You can use them in your update tool logic.
 
+            // work with xmlFilePath
+            if (!string.IsNullOrEmpty(xmlFilePath))
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    if (!File.Exists(xmlFilePath))
+                    {
+                        Console.WriteLine($"CSV file not exist: {xmlFilePath}");
+                        return;
+                    }
+
+                    // Load the XML file
+                    XmlDocument xmlDocument = new XmlDocument();
+                    xmlDocument.Load(xmlFilePath); // Replace with the actual path to your XML file
+
+                    // Get the root element (in this case, the "Drivers" element)
+                    XmlElement rootElement = xmlDocument.DocumentElement!;
+
+                    string make;
+                    string model;
+                    string updated;
+
+                    // Loop through each "Model" element under the "Drivers" element
+                    foreach (XmlElement modelElement in rootElement.GetElementsByTagName("Model"))
+                    {
+                        // Get the attribute values of each "Model" element
+                        string systemSKU = modelElement.GetAttribute("SystemSKU");
+                        string productName = modelElement.GetAttribute("ProductName");
+                        string displayName = modelElement.GetAttribute("DisplayName");
+                        string supportedBuilds = modelElement.GetAttribute("SupportedBuilds");
+
+                        make = "Microsoft";
+                        model = displayName;
+                        updated = DateTime.UtcNow.ToString("MM/dd/yyyy");
+
+                        string thisUid = $"{oem}::{make}::{model}";
+
+                        // Define the pattern for characters to be replaced
+                        string pattern = @"[^a-zA-Z_0-9:]";
+
+                        // Replace spaces with underscores
+                        thisUid = thisUid.Replace(" ", "_");
+
+                        // Replace characters that match the pattern with an empty string
+                        thisUid = Regex.Replace(thisUid, pattern, "");
+
+                        DriverCore thisDriverCore = new()
+                        {
+                            Id = 0,
+                            UID = thisUid,
+                            LastUpdated = DateTime.UtcNow,
+                            Make = make,
+                            Model = model,
+                            Oem = oem,
+                            SupportedWinRelease = new List<string>() { "Windows 10", "Windows 11" },
+                        };
+
+                        try
+                        {
+                            HttpResponseMessage response = await httpClient.GetAsync($"{apiEndpoint}/api/DriverCore/uid/{thisUid}");
+                            response.EnsureSuccessStatusCode();
+
+                            Console.WriteLine($"Found UID: {thisDriverCore.UID}");
+
+                            // read the response as a string
+                            string responseContent = await response.Content.ReadAsStringAsync();
+
+                            // deserialize
+                            List<DriverCore> drivers = JsonSerializer.Deserialize<List<DriverCore>>(responseContent)!;
+
+                            if (drivers.Count > 1)
+                            {
+                                Console.WriteLine("More than 1 driverCore error, needs to be investigated");
+                                continue;
+                            }
+
+                            if (drivers.Count < 1)
+                            {
+                                Console.WriteLine("Less than 1 driverCore error, needs to be investigated");
+                                continue;
+                            }
+
+                            DateTime dateTimeLastUpdated = DateTime.ParseExact(updated, "MM/dd/yyyy", null);
+
+                            thisDriverCore.Id = drivers[0].Id;
+
+                            string apiUpdateDriverCoreUrl = $"{apiEndpoint}/api/DriverCore/{thisDriverCore.Id}";
+
+                            try
+                            {
+                                HttpResponseMessage updateResponse = await httpClient.PutAsync(apiUpdateDriverCoreUrl, new StringContent(JsonSerializer.Serialize(thisDriverCore), Encoding.UTF8, "application/json"));
+
+                                if (updateResponse.IsSuccessStatusCode)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.Write("DriverCore updated: ");
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine(thisDriverCore.UID);
+                                    Console.ResetColor();
+                                }
+                                else if (updateResponse.StatusCode == HttpStatusCode.NoContent)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.Write("DriverCore updated: ");
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    Console.WriteLine(thisDriverCore.UID);
+                                    Console.ResetColor();
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"Error: {updateResponse.StatusCode}");
+                                    Console.WriteLine($"Unable to update DriverCore: {thisDriverCore.UID}");
+                                    Console.WriteLine($"uDriverCore: {JsonSerializer.Serialize(thisDriverCore)}");
+                                    continue;
+                                }
+                            }
+                            catch (HttpRequestException ex)
+                            {
+                                Console.WriteLine($"Error: {ex.Message}");
+                                Console.WriteLine($"Unable to update DriverCore: {thisDriverCore.UID}");
+                                Console.WriteLine($"uDriverCore: {JsonSerializer.Serialize(thisDriverCore)}");
+                                continue;
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error: {ex.Message}");
+
+                            string apiNewDriverCoreUrl = $"{apiEndpoint}/api/DriverCore";
+
+                            try
+                            {
+                                HttpResponseMessage newRecordResponse = await httpClient.PostAsync(apiNewDriverCoreUrl, new StringContent(JsonSerializer.Serialize(thisDriverCore), Encoding.UTF8, "application/json"));
+                                newRecordResponse.EnsureSuccessStatusCode();
+                                Console.ForegroundColor = ConsoleColor.Cyan;
+                                Console.Write("Posted new DriverCore: ");
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                Console.WriteLine(thisDriverCore.UID);
+                                Console.ResetColor();
+                            }
+                            catch (HttpRequestException ex2)
+                            {
+                                Console.WriteLine($"Error: {ex2.Message}");
+                                Console.WriteLine(JsonSerializer.Serialize(thisDriverCore));
+                            }
+
+                            continue;
+                        }
+                    }
+                }
+            }
             
             // work with csvFilePath
             if (!string.IsNullOrEmpty(csvFilePath))
